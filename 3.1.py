b82b94fad430057ee879d67b32aab1f1479f6365
Алексей Демкин <doc55514@gmail.com>
2024-05-29 21:17:12 +0300

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -13,19 +13,14 @@ fourier = np.zeros(len(freq_values), dtype=complex)
 for v in range(len(freq_values)):
     fourier[v] = np.trapz(data * np.exp(-1j * 2 * np.pi * freq_values[v] * np.arange(len(data)) / rate))
 
-# Найти индексы трех самых высоких значений в модуле Фурье-образа
+
 top_indices = np.argsort(np.abs(fourier))[-3:]
 
+top_frequencies = freq_values[top_indices]
+print("Три самых высоких частоты:", top_frequencies)
 plt.figure()
 plt.plot(freq_values, np.abs(fourier), color='blue')
 plt.xlabel('Частота (Гц)')
 plt.ylabel('|f(ν)|')
 plt.title('График модуля Фурье-образа')
-
-# Вывести тройку самых высоких частот
-for index in top_indices:
-    frequency = freq_values[index]
-    magnitude = np.abs(fourier[index])
-    plt.annotate(f'({frequency:.2f} Гц, {magnitude:.2f})', (frequency, magnitude), textcoords="offset points", xytext=(0,10), ha='center')
-
 plt.show()
